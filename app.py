31c96566e48d28a910d43e2e3fbd9e67ce54d65c
Martin Delgado <78776395+MartinAdolfoDelgadoHuayhua@users.noreply.github.com>
2025-04-24 18:23:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,44 +20,44 @@ if grupos_file and datos_file:
     try:
         grupos_df = pd.read_excel(grupos_file)
         user_df = pd.read_excel(datos_file)
-        user_df = user_df[['NOMBRE CENTRO COSTO','CODIGO RQ','PUESTO REQUERIDO']]
-        grupos_df = grupos_df[['NOMBRE CENTRO COSTO','cluster']]
+        user_df = user_df[['NUMERO CENTRO COSTO','CODIGO RQ','PUESTO REQUERIDO']]
+        grupos_df = grupos_df[['NUMERO CENTRO COSTO','cluster']]
 
-        user_df = user_df[user_df['PUESTO REQUERIDO'].isin([])
- 
+        # Se hace el filtrado de los puestos tomados en cuenta para el experimento
+        user_df = user_df[user_df['PUESTO REQUERIDO'].isin(['ASESOR DE NEGOCIOS 2', 'ASESOR DE NEGOCIOS 1','ASESOR DE NEGOCIOS 3','ASESOR DE NEGOCIOS  3  ','ASESOR DE NEGOCIOS  1  ',
+       'ASESOR DE NEGOCIOS  SENIOR', 'ASESOR DE NEGOCIOS  2  ']).reset_index(drop=True)
+
         # Validar columnas mínimas
-        if 'NOMBRE CENTRO COSTO' not in grupos_df.columns or 'NOMBRE CENTRO COSTO' not in user_df.columns:
-            st.error("Ambos archivos deben tener la columna 'NOMBRE CENTRO COSTO'.")
+        if 'NUMERO CENTRO COSTO' not in grupos_df.columns or 'NUMERO CENTRO COSTO' not in user_df.columns:
+            st.error("El archivo de requerimiento no tiene la columna 'NUMERO CENTRO COSTO'.")
         else:
             # Hacer merge por agencia
-            df = pd.merge(user_df, grupos_df, on="NOMBRE CENTRO COSTO", how="left")
+            df = pd.merge(user_df, grupos_df, on="NUMERO CENTRO COSTO", how="left")
  
-            if 'NOMBRE CENTRO COSTO' not in df.columns:
-                st.error("El archivo base debe contener la columna 'NOMBRE CENTRO COSTO'.")
-            else:
-                def asignar_marcas(grupo):
-                    n = len(grupo)
-                    mitad = n // 2
-                    marcas = ["Adam Milo"] * mitad + ["Manpower"] * (n - mitad)
-                    random.seed(2025)
-                    random.shuffle(marcas)
-                    grupo["Prueba"] = marcas
-                    return grupo
+            
+            def asignar_marcas(grupo):
+                n = len(grupo)
+                mitad = n // 2
+                marcas = ["Adam Milo"] * mitad + ["Manpower"] * (n - mitad)
+                random.seed(2025)
+                random.shuffle(marcas)
+                grupo["Prueba"] = marcas
+                return grupo
  
-                resultado_df = df.groupby("cluster", group_keys=False).apply(asignar_marcas)
- 
-                st.success("¡Asignación completada!")
- 
-                st.dataframe(resultado_df)
- 
-                # Preparar para descarga
-                output = BytesIO()
-                resultado_df.to_excel(output, index=False)
-                st.download_button(
-                    label="Descargar Excel con marcas asignadas",
-                    data=output.getvalue(),
-                    file_name="resultado_marcas.xlsx",
-                    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                )
+            resultado_df = df.groupby("cluster", group_keys=False).apply(asignar_marcas)
+
+            st.success("¡Asignación completada!")
+
+            st.dataframe(resultado_df)
+
+            # Preparar para descarga
+            output = BytesIO()
+            resultado_df.to_excel(output, index=False)
+            st.download_button(
+                label="Descargar Excel con marcas asignadas",
+                data=output.getvalue(),
+                file_name="resultado_marcas.xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
     except Exception as e:
         st.error(f"Error al procesar los archivos: {e}")
